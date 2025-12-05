c3bcb2aa95c17510597c1120b9b5735c9e5d405c
Anannya30 <anannyaDevale30@gmail.com>
2025-12-05 11:58:02 +0530

    adding chart

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,21 @@ import 'package:expense_tracker/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181),);
 
-var kDarkColorScheme = ColorScheme.fromSeed(brightness: Brightness.dark, Color.fromARGB(255, 5, 99, 125),);
+var kDarkColorScheme = ColorScheme.fromSeed(brightness: Brightness.dark,seedColor:  Color.fromARGB(255, 5, 99, 125),);
 void main(){
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
         colorScheme:kDarkColorScheme,
         cardTheme: CardThemeData().copyWith(
-          color: kColorScheme.secondaryContainer,
+          color: kDarkColorScheme.secondaryContainer,
           margin: EdgeInsets.symmetric(horizontal:16 , vertical: 8 ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
         ),
       ),
       theme: ThemeData(
